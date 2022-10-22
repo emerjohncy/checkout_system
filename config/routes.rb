b2993d148f2c6afa9267ad472ed96f3f168ce24e
Emer John Niones <nionesemerjohn@gmail.com>
2022-10-23 05:40:05 +0800

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "items#index"
   
   get "/catalog" => "items#index",
     as: "catalog"
