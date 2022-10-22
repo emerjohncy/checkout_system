ee4508ca9d266f916e998ab384174cfb867c83d7
Emer John Niones <nionesemerjohn@gmail.com>
2022-10-22 18:29:10 +0800

    add validation uniqueness on model Item sku

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
-    validates :sku, presence: true
+    validates :sku, presence: true,
+                    uniqueness: true
     validates :name, presence: true
     validates :price, presence: true
 end
