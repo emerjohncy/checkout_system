4fcad266fa3b9b24e3454910b778d3207639bfb7
Emer John Niones <nionesemerjohn@gmail.com>
2022-10-22 18:26:39 +0800

    add new and create methods on Items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,24 @@ class ItemsController < ApplicationController
     def index
         @items = Item.all
     end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+
+        if @item.save
+            redirect_to catalog_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:sku, :name, :price)
+    end
 end
