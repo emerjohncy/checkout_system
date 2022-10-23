45af6c91bd449309cd47eb4d844c1d9f579ce2e0
Emer John Niones <nionesemerjohn@gmail.com>
2022-10-23 10:17:40 +0800

    fix error

--- app/models/bulk_order.rb
+++ app/models/bulk_order.rb
@@ -1,5 +1,5 @@
 class BulkOrder < ApplicationRecord
-    validates :integer, presence: true
+    validates :min_quantity, presence: true
     validates :adjusted_price, presence: true
 
     belongs_to :item
