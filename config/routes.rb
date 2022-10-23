cab7ae8f244bb51a4f900546e71c3d7a3589aecb
Emer John Niones <nionesemerjohn@gmail.com>
2022-10-24 00:06:48 +0800

    change root path to cart path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "items#index"
+  root "cart#show"
   
   # Item Routes
   get "/catalog" => "items#index",
